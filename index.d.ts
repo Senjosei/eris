b89163366d3562da0be9e0510effaba38ec852a3
Linker-123 <Linker-123@users.noreply.github.com>
2022-02-10 18:02:10 +0100

    ts lint

--- index.d.ts
+++ index.d.ts
@@ -2946,7 +2946,7 @@ declare namespace Eris {
     editOriginalMessage(content: string | InteractionEditContent, file?: FileContent | FileContent[]): Promise<Message>;
     getOriginalMessage(): Promise<Message>
   }
-  
+
   interface ComponentInteractionButtonData {
     component_type: Constants["ComponentTypes"]["BUTTON"];
     custom_id: string;
